7d74f77da72399dd56fc119a12816fcfb6bff066
Hisuian Zoroark <96159984+HisuianZoroark@users.noreply.github.com>
2024-07-14 20:37:17 -0400

    Basic Framework Done

--- bot.js
+++ bot.js
@@ -1,2 +1,35 @@
 const Tmi = require('tmi.js');
-const Showdown = require('ps-client');
+const PSClient = require('ps-client');
+global.Config = require('./config/config.js');
+
+// Connect to PS!
+const PS = new PSClient.Client({
+	username: Config.Showdown.username,
+	password: Config.Showdown.password,
+	avatar: Config.Showdown.avatar,
+	debug: true,
+});
+PS.connect();
+
+// Connect to Twitch
+const Twitch = new Tmi.Client({
+	options: {debug: true},
+	identity: {
+		username: Config.Twitch.username,
+		password: Config.Twitch.oauth,
+	},
+	channels: [Config.Twitch.streamer],
+});
+Twitch.connect();
+
+PS.on('message', message => {
+	if (message.isIntro) return;
+	if (message.content === 'Ping!') return message.reply('Pong!');
+});
+
+Twitch.on('message', (channel, tags, message, self) => {
+	if (self) return;
+	if (message.toLowerCase() === '!hello') {
+		Twitch.say(channel, `@${tags.username}, heya!`);
+	}
+});
---
+++ config/config-example.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.Showdown = {
+  username: '',
+  password: '',
+  avatar: '',
+};
+
+exports.Twitch = {
+  username: '',
+  oauth: '',
+  streamer: '',
+  sysops: [],
+  prefix: '',
+};
