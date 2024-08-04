854e6de105459d8f4c9e033eeb20c161cf4c7717
Hisuian Zoroark <96159984+HisuianZoroark@users.noreply.github.com>
2024-08-03 23:52:37 -0400

    Remove ps-client package, make PS bot from scratch

--- bot.js
+++ bot.js
@@ -1,15 +1,12 @@
 const Tmi = require('tmi.js');
-const PSClient = require('ps-client');
+const WebSocket = require('ws');
+const fs = require('fs');
+const fetch = require('node-fetch');
 global.Config = require('./config/config.js');
 
 // Connect to PS!
-const PS = new PSClient.Client({
-	username: Config.Showdown.username,
-	password: Config.Showdown.password,
-	avatar: Config.Showdown.avatar,
-	debug: true,
-});
-PS.connect();
+const PSClient = new WebSocket('wss://sim3.psim.us/showdown/websocket');
+
 
 // Connect to Twitch
 const Twitch = new Tmi.Client({
@@ -22,14 +19,54 @@ const Twitch = new Tmi.Client({
 });
 Twitch.connect();
 
-PS.on('message', message => {
-	if (message.isIntro) return;
-	if (message.content === 'Ping!') return message.reply('Pong!');
-});
-
 Twitch.on('message', (channel, tags, message, self) => {
 	if (self) return;
 	if (message.toLowerCase() === '!hello') {
 		Twitch.say(channel, `@${tags.username}, heya!`);
 	}
 });
+
+PSClient.on('open', function open(connection) {
+	console.log('Connected to PS!');
+});
+
+PSClient.on('message', function message(data) {
+	const bits = data.toString().split('|');
+	if (bits[1] === 'challstr') {
+		// console.log('received: %s', bits);
+		PSlogin(bits);
+	} else if (bits[1] === 'request') {
+		room = bits[0];
+		if (bits[2].length > 0) {
+			reqData = JSON.parse(bits[2]);
+			console.log(reqData);
+		} else {
+			reqData = false;
+			room = false;
+		}
+	}
+});
+
+async function PSlogin(bits) {
+	try {
+		const body = `act=login&name=${Config.Showdown.username}&pass=${Config.Showdown.password}&challengekeyid=${bits[2]}&challenge=${bits[3]}`;
+		const response = await fetch('https://play.pokemonshowdown.com/action.php', {
+			method: 'post',
+			body: body,
+			headers: {"Content-Type": "application/x-www-form-urlencoded", "Content-Length": body.length},
+		});
+		let data = await response.text();
+		if (data.charAt(0) === ']') {
+			data = JSON.parse(data.substring(1));
+			PSClient.send(`|/trn ${Config.Showdown.username},0,${data.assertion}`);
+			PSClient.send(`|/avatar ${Config.Showdown.avatar}`);
+			console.log(`Logged into PS! Username: ${Config.Showdown.username}`);
+		} else {
+			console.log(`There might be an error with the account you're trying to access, please try again.`);
+			return;
+		}
+	} catch (e) {
+		console.log(e);
+		return;
+	}
+}
