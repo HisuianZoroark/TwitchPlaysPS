b95e32ab4a571220aa94f18215a5c8b5739a6885
HiZo <96159984+HisuianZoroark@users.noreply.github.com>
2024-09-21 22:51:30 -0400

    i started this again

--- bot.js
+++ bot.js
@@ -1,14 +1,17 @@
+'use strict';
 const Tmi = require('tmi.js');
-const WebSocket = require('ws');
-const ReconnectingWebSocket = require('websocket-reconnect');
-const fs = require('fs');
-const fetch = require('node-fetch');
+const PSClient = require('ps-client');
 global.Config = require('./config/config.js');
 
 // Connect to PS!
-const PS = new ReconnectingWebSocket.WsReconnect({ reconnectDelay: 5000 });
-PS.open('wss://sim3.psim.us/showdown/websocket');
+const Ps = new PSClient.Client({
+	username: Config.Showdown.username,
+	password: Config.Showdown.password,
+	avatar: Config.Showdown.avatar,
+	debug: true,
+});
 
+Ps.connect();
 
 // Connect to Twitch
 const Twitch = new Tmi.Client({
@@ -27,48 +30,3 @@ Twitch.on('message', (channel, tags, message, self) => {
 		Twitch.say(channel, `@${tags.username}, heya!`);
 	}
 });
-
-PS.on('open', function open(connection) {
-	console.log('Connected to PS!');
-});
-
-PS.on('message', function message(data) {
-	const bits = data.toString().split('|');
-	if (bits[1] === 'challstr') {
-		// console.log('received: %s', bits);
-		PSlogin(bits);
-	} else if (bits[1] === 'request') {
-		room = bits[0];
-		if (bits[2].length > 0) {
-			reqData = JSON.parse(bits[2]);
-			console.log(reqData);
-		} else {
-			reqData = false;
-			room = false;
-		}
-	}
-});
-
-async function PSlogin(bits) {
-	try {
-		const body = `act=login&name=${Config.Showdown.username}&pass=${Config.Showdown.password}&challengekeyid=${bits[2]}&challenge=${bits[3]}`;
-		const response = await fetch('https://play.pokemonshowdown.com/action.php', {
-			method: 'post',
-			body: body,
-			headers: {"Content-Type": "application/x-www-form-urlencoded", "Content-Length": body.length},
-		});
-		let data = await response.text();
-		if (data.charAt(0) === ']') {
-			data = JSON.parse(data.substring(1));
-			PS.send(`|/trn ${Config.Showdown.username},0,${data.assertion}`);
-			PS.send(`|/avatar ${Config.Showdown.avatar}`);
-			console.log(`Logged into PS! Username: ${Config.Showdown.username}`);
-		} else {
-			console.log(`There might be an error with the account you're trying to access, please try again.`);
-			return;
-		}
-	} catch (e) {
-		console.log(e);
-		return;
-	}
-}
