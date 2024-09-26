bcf3198a8ccfd8f10ba9c135c257a1ad48488428
HiZo <96159984+HisuianZoroark@users.noreply.github.com>
2024-09-25 21:01:38 -0400

    AG Support!

--- battle.js
+++ battle.js
@@ -7,23 +7,39 @@ class Battle {
 		this.room = room;
 		this.team = team;
 		this.tally = new Map();
-		this.votecmds = {};
+		this.votecmds = new Map();
 		this.acceptingVotes = false;
 		this.timeout = 15;
 	}
 	genOptions(requestState) {
-		this.votecmds = {};
+		this.votecmds.clear();
 		let request = JSON.parse(requestState);
 		if (request.active && !request.forceSwitch) {
 			for (let y = 0; y < request.active.length; y++) {
 				for (let x = 0; x < request.active[y].moves.length; x++) {
 					if (request.active[y].moves[x].disabled) continue;
 					let move = request.active[y].moves[x].move;
-					this.votecmds[`move ${x + 1}`] = `/move ${x + 1}`;
-					this.votecmds[`move ${move.toLowerCase()}`] = `/move ${x + 1}`;
+					this.votecmds.set(`move ${x + 1}`, `/move ${x + 1}`);
+					if (this.votecmds.get(`move ${move.toLowerCase()}`)) {
+						for (let dupeCount = 1; dupeCount <= 24; dupeCount++) {
+							if (this.votecmds.get(`move ${move.toLowerCase()}${dupeCount}`)) continue;
+							this.votecmds.set(`move ${move.toLowerCase()}${dupeCount}`, `/move ${x + 1}`);
+							break;
+						}
+					} else {
+						this.votecmds.set(`move ${move.toLowerCase()}`, `/move ${x + 1}`);
+					}
 					if (request.active[y].canTerastallize) {
-						this.votecmds[`move ${x + 1} tera`] = `/move ${x + 1} terastallize`;
-						this.votecmds[`move ${move.toLowerCase()} tera`] = `/move ${x + 1} terastallize`;
+						this.votecmds.set(`move ${x + 1} tera`, `/move ${x + 1} terastallize`);
+						if (this.votecmds.get(`move ${move.toLowerCase()} tera`)) {
+							for (let dupeCount = 1; dupeCount <= 24; dupeCount++) {
+								if (this.votecmds.get(`move ${move.toLowerCase()}${dupeCount} tera`)) continue;
+								this.votecmds.set(`move ${move.toLowerCase()}${dupeCount} tera`, `/move ${x + 1} terastallize`);
+								break;
+							}
+						} else {
+							this.votecmds.set(`move ${move.toLowerCase()} tera`, `/move ${x + 1} terastallize`);
+						}
 					}
 				}
 			}
@@ -34,8 +50,17 @@ class Battle {
 			for (let x = 0; x < request.side.pokemon.length; x++) {
 				if (request.active && !request.teamPreview && x <= 0) continue;
 				this.votecmds[`switch ${x + 1}`] = `/${keyword} ${x + 1}`;
-				let pokemon = request.side.pokemon[x].ident.substring(4);
-				this.votecmds[`switch ${pokemon.toLowerCase()}`] = `/${keyword} ${x + 1}`;
+				let pokemon = request.side.pokemon[x].details.split(',')[0];
+				// this.votecmds[`switch ${pokemon.toLowerCase()}`] = `/${keyword} ${x + 1}`;
+				if (this.votecmds.get(`switch ${pokemon.toLowerCase()}`)) {
+					for (let dupeCount = 1; dupeCount <= 24; dupeCount++) {
+						if (this.votecmds.get(`switch ${pokemon.toLowerCase()}${dupeCount}`)) continue;
+						this.votecmds.set(`switch ${pokemon.toLowerCase()}${dupeCount}`, `/${keyword} ${x + 1}`);
+						break;
+					}
+				} else {
+					this.votecmds.set(`switch ${pokemon.toLowerCase()}`, `/${keyword} ${x + 1}`);
+				}
 			}
 		}
 	}
@@ -67,8 +92,8 @@ class Battle {
 	async submitVote(username, vote) {
 		console.log(vote);
 		let sanitizedvote = vote.toLowerCase().trim();
-		let realvote = this.votecmds[sanitizedvote] || null;
-		console.log(this.votecmds[sanitizedvote]);
+		let realvote = this.votecmds.get(sanitizedvote) || null;
+		console.log(this.votecmds.get(sanitizedvote));
 		if (!realvote || !this.acceptingVotes) {
 			twitchChat(`@${username} your vote was not accepted.`);
 			return;
